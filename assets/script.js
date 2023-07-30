455633c28b137a2e0e92ee2d7be0eda7be63f061
Shubham Ashish <shubhamashish33>
2023-07-30 22:52:15 +0530

    light mode added

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   const popupContainer = document.querySelector(".popup-container");
+  const themeToggle = document.getElementById("theme-toggle");
 
   function hidePopupOnScroll() {
     popupContainer.style.opacity = "0";
+    themeToggle.style.opacity = "0";
 
     window.removeEventListener("scroll", hidePopupOnScroll);
 
@@ -12,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
   function showPopupOnTop() {
     if (window.scrollY === 0) {
       popupContainer.style.opacity = "1";
+      themeToggle.style.opacity = "1";
 
       window.removeEventListener("scroll", showPopupOnTop);
 
@@ -22,19 +25,36 @@ document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("scroll", hidePopupOnScroll);
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll(".collapsible");
 
-document.addEventListener("DOMContentLoaded", function() {
-    const sections = document.querySelectorAll(".collapsible");
-  
-    sections.forEach(section => {
-      const title = section.querySelector(".title");
-      const content = section.querySelector(".para");
-  
-      title.addEventListener("click", function() {
-        // Toggle the display of the content element
-        content.style.display = (content.style.display === "none") ? "block" : "none";
-      });
+  sections.forEach((section) => {
+    const title = section.querySelector(".title");
+    const content = section.querySelector(".para");
+
+    title.addEventListener("click", function () {
+      content.style.display =
+        content.style.display === "none" ? "block" : "none";
     });
   });
-  
-  
+});
+
+const themeToggle = document.getElementById("theme-toggle");
+const body = document.body;
+
+themeToggle.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  body.classList.add("theme-transition");
+
+  setTimeout(() => {
+    body.classList.remove("theme-transition");
+  }, 500);
+
+  const isDarkMode = body.classList.contains("dark");
+  localStorage.setItem("isDarkMode", isDarkMode);
+});
+
+const savedTheme = localStorage.getItem("isDarkMode");
+if (savedTheme === "true") {
+  body.classList.add("dark");
+}
